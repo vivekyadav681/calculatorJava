88dfea53a9c7095572490245563a5aa83a72000b
Vivek Yadav <vivekyadav00681@gmail.com>
2025-05-12 11:03:54 +0530

    commit init

---
+++ src/Calculator.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton clrButton, decButton, delButton, equButton;
+    JPanel panel;
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+    public Calculator() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+        frame.add(textField);
+
+        frame.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
